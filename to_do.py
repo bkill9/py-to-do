9ead1113d0bc902e0de4d7cff34457a39c62846d
Brenleigh Killpack <242260823+bkill9@users.noreply.github.com>
2026-09-03 13:28:43 -0600

    Outline functions with doc strings

---
+++ to_do.py
@@ -0,0 +1,66 @@
+def add_task(tasks):
+  """Allow the user to enter task data and add it to the task list.
+
+  Args:
+      tasks: A list of task dictionaries.
+
+  Returns:
+      None.
+  """
+  pass
+
+def view_tasks(tasks):
+  """Display all tasks in a readable format.
+
+  Args:
+      tasks: A list of task dictionaries to display.
+
+  Returns:
+      None.
+  """
+  pass
+
+def complete_task(tasks):
+  """Allow the user to select a task and mark it as complete.
+
+  The selected task's status is changed from False (incomplete)
+  to True (complete).
+
+  Args:
+      tasks: A list of task dictionaries to modify.
+
+  Returns:
+      None.
+  """
+  pass
+
+def remove_task(tasks):
+  """Allow the user to select a task and remove it from the task list.
+
+  Args:
+      tasks: A list of task dictionaries to modify.
+
+  Returns:
+      None.
+  """
+  pass
+
+def save_tasks(tasks):
+  """Allow the user to enter a JSON filename and write the task list to it.
+
+  Args:
+      tasks: A list of task dictionaries to write to a JSON file.
+
+  Returns:
+      None.
+  """
+  pass
+
+def load_tasks():
+  """Load task data from a JSON file.
+
+  Returns:
+      A list of dictionaries populated with task data.
+  """
+  pass
+
